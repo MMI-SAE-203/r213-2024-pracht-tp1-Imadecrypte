67acb99993aeac3f151d9b6ab35ec3dd4ee23ea0
Imadecrypte <130630779+Imadecrypte@users.noreply.github.com>
2024-04-12 14:30:06 +0200

    ajout des sections

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,31 @@
-
 <script setup lang="ts">
 //
 </script>
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
+  <section>
+    <button class="text-xl">bouton 1</button>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni assumenda suscipit sequi
+      commodi maiores nam consequatur exercitationem fuga reprehenderit, molestias corporis eos sed
+      incidunt iure blanditiis dolores, facere ratione odio!
+    </p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 2</button>
+    <p>
+      Ipsum commodi quo minus quisquam rem voluptatibus suscipit iusto blanditiis. Vero ab nobis
+      explicabo. Hic corrupti mollitia ducimus facere. Id mollitia soluta quaerat reiciendis dolores
+      harum eligendi quam consequuntur facilis?
+    </p>
+  </section>
+  <section>
+    <button class="text-xl">bouton 3</button>
+    <p>
+      In quidem iste consectetur aut, amet ratione corporis temporibus ab est molestias velit facere
+      explicabo repudiandae deserunt sint omnis rerum error ut officiis consequuntur sunt beatae
+      inventore. Illo, eum sunt?
+    </p>
+  </section>
 </template>
